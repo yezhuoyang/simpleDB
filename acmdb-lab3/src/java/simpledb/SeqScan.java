ac9305b095a50fb1e93d6894b78504247ddec42a
Zhuoyang ye <8412190+yezhuoyang@user.noreply.gitee.com>
2021-05-03 09:11:42 +0800

    Lab3 ex1.

--- acmdb-lab3/src/java/simpledb/SeqScan.java
+++ acmdb-lab3/src/java/simpledb/SeqScan.java
@@ -125,7 +125,10 @@ public class SeqScan implements DbIterator {
     }
 
     public void close() {
-        iterator=null;
+        if(iterator!=null){
+            iterator.close();
+            iterator=null;
+        }
     }
 
     public void rewind() throws DbException, NoSuchElementException,
